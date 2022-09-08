c49774870e6f110d8935c7a36a46998276758007
boardguy1024 <boardguy1024@gmail.com>
2022-09-08 14:08:57 +0900

    Initial Commit

---
+++ SwiftUI_CustomListWithSwipeToDelete/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_CustomListWithSwipeToDelete
+//
+//  Created by park kyung seok on 2022/09/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_CustomListWithSwipeToDelete/SwiftUI_CustomListWithSwipeToDeleteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CustomListWithSwipeToDeleteApp.swift
+//  SwiftUI_CustomListWithSwipeToDelete
+//
+//  Created by park kyung seok on 2022/09/08.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CustomListWithSwipeToDeleteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
